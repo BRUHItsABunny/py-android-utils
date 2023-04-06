0f5205e222f8bf450b0de4469c0e677f65438085
BRUHITsABunny <53124399+BRUHItsABunny@users.noreply.github.com>
2023-04-06 04:47:29 +0200

    add autocompletion

--- _resources/scripts/generate.bat
+++ _resources/scripts/generate.bat
@@ -2,6 +2,7 @@
 docker build -t protos .
 docker run --name="protos_run" protos
 docker cp protos_run:proto/proto-android-utils/android_utils_pb2.py ./androidutils/android_utils_pb2.py
+docker cp protos_run:proto/proto-android-utils/android_utils_pb2.pyi ./androidutils/android_utils_pb2.pyi
 docker rm protos_run
 docker rmi protos
 echo "BAT done"
